b3fbd2a563ff489b9240aa516e39003a8b29b4c6
Hai Dang <haiduc.dang91@gmail.com>
2018-01-09 18:27:36 +0100

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
-var request = require("request");
+let request = require("request");
 
+let today = new Date();
 
 var options = { method: 'POST',
     url: 'https://projectile.office.sevenval.de/projectile/start',
@@ -12,8 +13,8 @@ var options = { method: 'POST',
             jsenabled: '1',
             isAjax: '0',
             develop: '0',
-            login: '',
-            password: '' },
+            login: 'haiduc.dang',
+            password: 'He0MeuNT' },
     strictSSL: false //TODO: SSL Zertifizierung mit node.js
 };
 
@@ -30,7 +31,7 @@ request(options, function (error, response, body) {
     let cookie = temp.split(';')[0];
 
 
-      // getURL('GET', 'https://projectile.office.sevenval.de/projectile/start#%21/Intro', cookie);
+      // getURL('GET', 'https://projectile.office.sevenval.de/projectile/start#%21/Intro', cookie); d
 
     getEmployee(cookie).then(item=> {
         let bod = {};
@@ -44,7 +45,40 @@ request(options, function (error, response, body) {
 
         bod["Dock"]=  [ 'Area.TrackingArea', 'Area.ProjectManagementArea' ];
 
-        showJobList('POST','https://projectile.office.sevenval.de/projectile/gui5ajax?action=get&_dc=1515080349669', cookie, bod);
+
+         // showJobList('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=get&_dc=1515081239766', cookie, (function(){return bod})() , item);
+        console.log(item);
+
+        // TODO: synchronous design
+   /* let fun = () => {
+        return new Promise((resolve, reject)=> {
+            showJobList('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=get&_dc=1515081239766', cookie, (function(){return bod})() , item);
+            resolve();
+            //setEntryToday(cookie, item);
+           /!* PostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=get&_dc=1515081239766', cookie,{"Dock":["Area.TrackingArea"],"TimeTracker!^.|Default|Employee|1|475":["DayList","JobList","Begin","Favorites","TrackingRestriction","FilterCustomer","FilterProject"]}, function(result){
+                console.log(result["values"][item][2]["v"]);
+            } )*!/
+        })
+    }
+
+    fun().then(()=> { setEntryToday(cookie, item);
+        PostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=get&_dc=1515081239766', cookie,{"Dock":["Area.TrackingArea"],"TimeTracker!^.|Default|Employee|1|475":["DayList","JobList","Begin","Favorites","TrackingRestriction","FilterCustomer","FilterProject"]}, function(result){
+            console.log(result["values"][item][2]["v"]);
+        } )
+    })
+
+    fun();*/
+
+
+        // change calendar date to today
+        setEntryToday(cookie, item);
+        //   getDayListToday(cookie, item);
+
+        //view ListEntry for today
+        PostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=get&_dc=1515081239766', cookie,{"Dock":["Area.TrackingArea"],"TimeTracker!^.|Default|Employee|1|475":["DayList","JobList","Begin","Favorites","TrackingRestriction","FilterCustomer","FilterProject"]}, function(result){
+            console.log(result["values"][item][2]["v"]);
+        } )
+
     });
 
 });
@@ -84,7 +118,7 @@ function option (method, url, cookie, body){
     return options;
 }
 
-function showJobList(method, url, cookie, body){
+function showJobList(method, url, cookie, body, Employee){
     let options = option(method, url, cookie, body);
 
     request(options, function (error, response, body) {
@@ -92,15 +126,15 @@ function showJobList(method, url, cookie, body){
 
 
 
-        let EmplNumber = Object.keys(body["values"])[0].split('|');
+      /*  let EmplNumber = Object.keys(body["values"])[0].split('|');
         let tem = EmplNumber.splice(3,7);
 
-        /**
+        /!**
          *
          * @type {string}
          * first attribute of values:
-         */
-        let Employee = tem.join('|');
+         *!/
+        let Employee = tem.join('|');*/
 
         /**
          * get name and NO. of Employee
@@ -147,13 +181,23 @@ function showJobList(method, url, cookie, body){
 
 
 function normalPostURL(method, url, cookie, body){
-   option( method, url, cookie, body);
+   let options = option( method, url, cookie, body);
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        console.log(body);
+        // console.log(body);
     });
 }
+
+function PostURL(method, url, cookie, body, callback){
+   let options = option( method, url, cookie, body);
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        callback(body);
+    });
+}
+
 
 function getEmployee(cookie){
     return new Promise((resolve) => {
@@ -170,9 +214,58 @@ function getEmployee(cookie){
 
 }
 
-function saveEntry(cookie, time, note, paket){
+function getDayListToday( cookie, employee){
 
-   // normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=commit&_dc=1515080248606', cookie, {"values":{"+.|DayList|22|TimeTracker!^.|Default|Employee|1|357":[{"n":"Time","v":time}]}} )
-   // normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=options&_dc=1515080619255', cookie, {"+.|DayList|22|TimeTracker!^.|Default|Employee|1|357":["What"]}        )
+    PostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=get&_dc=1515081239766', cookie, (() => {
+        let obj = {};
+        obj["Dock"] = ["Area.TrackingArea"];
+        obj[employee]= ["DayList","JobList","Begin","Favorites","TrackingRestriction","FilterCustomer","FilterProject"];
+        return obj;
+    })(), (result) => {
+        console.log(result["values"][employee][2]["v"]);
+    });
 
 }
+
+function saveEntry(cookie, time, note, paket){
+
+    // time
+    normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=commit&_dc=1515080248606', cookie,  {"values":{"+.|DayList|2|TimeTracker!^.|Default|Employee|1|357":[{"n":"Time","v":time}]}} );
+    normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=options&_dc=1515080619255', cookie, {"+.|DayList|2|TimeTracker!^.|Default|Employee|1|357":["What"]}        );
+    // Auswahl des Projektes
+    normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=commit&_dc=1515080624305', cookie, {"values":{"+.|DayList|2|TimeTracker!^.|Default|Employee|1|357":[{"n":"What","v":"2354-425"}]}});
+    // Bemerkung
+
+    /*normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=commit&_dc=1515080659058', cookie,  {"values":{"+.|DayList|22|TimeTracker!^.|Default|Employee|1|357":[{"n":"Note","v":"Automatisierung Projectile mit Node.js"}]}});
+    // Speichern */
+
+
+}
+
+function setEntryToday(cookie, Employee){
+
+    normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=commit&_dc=1515501823869', cookie, (function(){
+        let obj ={} ;
+        obj.values = {};
+        obj.values[Employee] = [{"n":"Begin","v": today}];
+        return obj;
+    })());
+
+    normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=options&_dc=1515501824360', cookie,  (function(){
+        let obj = {};
+        obj[Employee] = ["FilterProject","FilterCustomer"];
+        return obj})());
+
+    normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=commit&_dc=1515501823869', cookie, (function(){
+        let obj ={"gui_TimeTracker_JobList":{"columns":[{"di":"gridcolumn-1287"},{"di":"rownumberer-1288","width":23},{"di":"_actions","width":46},{"di":"ProcessNumberWithLink"},{"di":"JobNameWithLink"},{"di":"ProjectNameWithLink"},{"di":"Customer"},{"di":"DueTime"},{"di":"Time"},{"di":"TotalTime"},{"di":"TimeToCompletion"},{"di":"Favorite","width":30}],"filters":{}}}
+        return obj;
+    })());
+
+
+
+
+    // normalPostURL('POST', 'https://projectile.office.sevenval.de/projectile/gui5ajax?action=state&_dc=1515501825072', cookie, {"gui_TimeTracker_JobList":{"columns":[{"di":"gridcolumn-1287"},{"di":"rownumberer-1288","width":23},{"di":"_actions","width":46},{"di":"ProcessNumberWithLink"},{"di":"JobNameWithLink"},{"di":"ProjectNameWithLink"},{"di":"Customer"},{"di":"DueTime"},{"di":"Time"},{"di":"TotalTime"},{"di":"TimeToCompletion"},{"di":"Favorite","width":30}],"filters":{}}});
+
+
+}
+
